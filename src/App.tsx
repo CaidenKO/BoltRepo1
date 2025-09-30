bd382495bef7709278f666e719a5ad70c6cfb397
CaidenKO <caidenknorr@outlook.com>
2025-09-30 09:56:52 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}import React from 'react';
+| {"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}import React from 'react';
+       ^
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
-
-export default App;
+ function App() {
+   return (
